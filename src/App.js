e7b03398ecfa3415b2e2c65f1980a59620ec7fec
PROLCY <ckd248@naver.com>
2022-11-17 15:27:18 +0900

    2022/11/17 socket.io 추가 및 웹소켓 테스트

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import MakerPage from './pages/MakerPage';
 import SolverPage from './pages/SolverPage';
 import LoadPage from './pages/LoadPage';
+import TestPage from './pages/TestPage';
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Route path="/" element={<MakerPage />} />
             <Route path="/solve/:maker" element={<SolverPage />} />
             <Route path="/load/" element={<LoadPage />}/>
+            <Route path="/test/" element={<TestPage />}/>
         </Routes>
     );
 };
